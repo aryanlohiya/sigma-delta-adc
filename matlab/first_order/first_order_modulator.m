d3a6efda45f2e9679a7dd2249186984b3fc43a6c
aryanlohiya <aryan.lohiya@iitgn.ac.in>
2026-07-22 18:20:25 +0530

    Add first-order modulator framework

---
+++ matlab/first_order/first_order_modulator.m
@@ -0,0 +1,16 @@
+function bitstream = first_order_modulator(inputSignal)
+% =========================================================================
+% first_order_modulator
+%
+% Implements a first-order Sigma-Delta modulator.
+%
+% Inputs:
+%   inputSignal  - Normalized analog input sequence
+%
+% Outputs:
+%   bitstream    - 1-bit Sigma-Delta output
+% =========================================================================
+
+% Implementation will be added in the next commit.
+
+end
