b0297936575e25a2c04f6b81a44a92ad0beb59e1
aryanlohiya <aryan.lohiya@iitgn.ac.in>
2026-07-25 21:29:19 +0530

    Add FFT analysis for first-order Sigma-Delta ADC

---
+++ matlab/first_order/fft_analysis.m
@@ -0,0 +1,39 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FFT Analysis of First-Order Sigma-Delta Modulator
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear;
+clc;
+close all;
+
+%% Run Modulator
+
+run_first_order
+
+%% FFT
+
+Nfft = length(bitstream);
+
+Y = fft(bitstream);
+
+Y = abs(Y)/Nfft;
+
+Y = Y(1:Nfft/2);
+
+f = (0:Nfft/2-1)*(Fs/Nfft);
+
+%% Plot
+
+figure
+
+plot(f/1000,20*log10(Y),'LineWidth',1.5)
+
+grid on
+
+xlabel('Frequency (kHz)')
+
+ylabel('Magnitude (dB)')
+
+title('FFT of Sigma-Delta Bitstream')
+
+xlim([0 20])
