2f634db6de3a61bdb57050bc114d259cbd953818
aryanlohiya <aryan.lohiya@iitgn.ac.in>
2026-07-31 03:05:13 +0530

    Add testbench for top-level decimation chain

---
+++ verilog/filters/tb_decimation_chain.v
@@ -0,0 +1,69 @@
+`timescale 1ns/1ps
+
+module tb_decimation_chain;
+
+reg clk;
+reg rst;
+reg bitstream;
+
+wire signed [15:0] data_out;
+
+decimation_chain uut(
+
+    .clk(clk),
+    .rst(rst),
+    .bitstream(bitstream),
+    .data_out(data_out)
+
+);
+
+initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+end
+
+initial begin
+
+    rst = 1;
+    bitstream = 0;
+
+    #20;
+    rst = 0;
+
+    bitstream=1; #10;
+    bitstream=0; #10;
+    bitstream=1; #10;
+    bitstream=1; #10;
+    bitstream=0; #10;
+    bitstream=0; #10;
+    bitstream=1; #10;
+    bitstream=0; #10;
+
+    bitstream=1; #10;
+    bitstream=1; #10;
+    bitstream=1; #10;
+    bitstream=0; #10;
+    bitstream=0; #10;
+    bitstream=1; #10;
+    bitstream=0; #10;
+    bitstream=1; #10;
+
+    #100;
+
+    $finish;
+
+end
+
+initial begin
+
+    $monitor("Time=%0t Bit=%b Output=%0d",
+             $time, bitstream, data_out);
+
+end
+
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+end
+
+endmodule
